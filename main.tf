7a3ac8b65b5fa05f84d87b9e16947348ea7fffea
Will Brooke <56451407+redbrooke@users.noreply.github.com>
2026-01-15 11:51:08 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -103,13 +103,4 @@ QUERY
   enabled                    = true
 }
 
-#####################################################################
-# Create storage account for boot diagnostics
 
-resource "azurerm_storage_account" "honeypot_storage_account" {
-  name                     = "bootlogs"
-  location            = azurerm_resource_group.HoneyProjectPots.location
-  resource_group_name = azurerm_resource_group.HoneyProjectPots.name
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
