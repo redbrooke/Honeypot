46ec1e69c43571b2f41bc161b9afc83b7c55b972
Will Brooke <56451407+redbrooke@users.noreply.github.com>
2026-01-27 16:59:07 +0000

    started new pot type

--- SSHPot/main.tf
+++ SSHPot/main.tf
@@ -1,1 +1,22 @@
-# placeholder
+
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "SSHPotGroup" {
+  name     = "SSHGroup"
+  location = "ukwest"
+  tags = {"Project" = "Honeypot"}
+}
